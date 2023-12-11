707cd22ebb656e038a42952a92c928bc6b2e9b23
BaraMaskova <baramaskova@seznam.cz>
2023-12-11 13:37:41 +0100

    Add file secondary_table.sql

---
+++ secondary_table.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE TABLE t_barbora_maskova_project_SQL_secondary_final AS
+SELECT
+	c.country ,
+	e.`year` ,
+	e.population,
+	e.gini ,
+	e.GDP
+FROM countries c
+JOIN economies e
+	on c.country = e.country
+	WHERE 1=1
+		AND c.continent = 'Europe'
+		AND e.`year` BETWEEN 2006 AND 2018
+ORDER BY c.`country`, e.`year`;
+
+SELECT * FROM t_barbora_maskova_project_SQL_secondary_final
+WHERE country = 'Czech Republic';
